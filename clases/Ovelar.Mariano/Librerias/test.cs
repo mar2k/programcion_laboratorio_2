b28d7b1983e8065258c74e9c4ca72c58e05708f3
alumno <alumno>
2019-03-26 12:03:22 -0300

    primer commit

---
+++ clases/Ovelar.Mariano/Librerias/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librerias
+{
+    public class test
+    {
+        public static string Saludar()
+        {
+            return "hola mundo";
+        }
+    }
+}
---
+++ clases/Ovelar.Mariano/proyecto03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Librerias;
+
+namespace proyecto03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test.Saludar());
+
+            Console.ReadKey();
+        }
+    }
+}
